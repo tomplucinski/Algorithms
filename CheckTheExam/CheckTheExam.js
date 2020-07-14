dbfa40216e9ccbcf1c80a15e47d09c08db528027
Tom Plucinski <tapluci1@gmail.com>
2020-07-14 08:45:00 -0500

    CheckTheExam

---
+++ CheckTheExam/CheckTheExam.js
@@ -0,0 +1,16 @@
+function checkExam(array1, array2) {
+    let score = 0;
+    
+    for (let i = 0; i < array1.length; i++) {
+      
+      if (array1[i] === array2[i]) {
+        score += 4
+      }
+      
+      if (array1[i] !== array2[i] && array2[i] !== "") {
+        score -= 1
+      }
+    }
+    
+    return score > 0 ? score : 0
+  }
