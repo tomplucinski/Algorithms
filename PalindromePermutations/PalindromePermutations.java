7ee57751750e853198bf70e153fa8bdbdadc08a7
Tom Plucinski <tapluci1@gmail.com>
2020-11-14 17:16:02 -0600

    PalindromePermutations

---
+++ PalindromePermutations/PalindromePermutations.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+
+public class PalindromePermutation {
+
+    public static boolean hasPalindromePermutation(String theString) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        return false;
+    }
+}
