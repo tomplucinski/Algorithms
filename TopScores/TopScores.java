97a1a2e388f1e5bba2874d35a39229cc89e03891
Tom Plucinski <tapluci1@gmail.com>
2020-11-21 13:46:42 -0600

    TopScores update

--- TopScores/TopScores.java
+++ TopScores/TopScores.java
@@ -2,6 +2,24 @@ public class TopScores {
 
     public static int[] sortScores(int[] unorderedScores, int highestPossibleScore) {
 
-        return unorderedScores;
+        int[] scoreCounts = new int[highestPossibleScore + 1];
+
+        for (int score : unorderedScores) {
+            scoreCounts[score]++;
+        }
+
+        int[] sortedScores = new int[unorderedScores.length];
+        int currentSortedIndex = 0;
+
+        for (int score = highestPossibleScore; score >= 0; score--) {
+            int count = scoreCounts[score];
+
+            for (int occurrence = 0; occurrence < count; occurrence++) {
+                sortedScores[currentSortedIndex] = score;
+                currentSortedIndex++;
+            }
+        }
+
+        return sortedScores;
     }
 }
