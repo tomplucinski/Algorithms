3debad1db9b9f78c80fe72837b23f503a804b701
Tom Plucinski <tapluci1@gmail.com>
2020-11-22 22:02:33 -0600

    FindHighestProduct update

--- FindHighestProduct/FindHighestProduct.java
+++ FindHighestProduct/FindHighestProduct.java
@@ -1,7 +1,41 @@
 public class FindHighestProduct {
 
-    public static int highestProductOf3(int[] intArray) {
+    public static int highestProductOf3(int[] arrayOfInts) {
 
-        return 0;
+        if (arrayOfInts.length < 3) {
+            throw new IllegalArgumentException("There are less than 3 items!");
+        }
+
+        int highest = Math.max(arrayOfInts[0], arrayOfInts[1]);
+        int lowest  = Math.min(arrayOfInts[0], arrayOfInts[1]);
+
+        int highestProductOf2 = arrayOfInts[0] * arrayOfInts[1];
+        int lowestProductOf2  = arrayOfInts[0] * arrayOfInts[1];
+        int highestProductOf3 = arrayOfInts[0] * arrayOfInts[1] * arrayOfInts[2];
+
+        for (int i = 2; i < arrayOfInts.length; i++) {
+            int current = arrayOfInts[i];
+
+            highestProductOf3 = Math.max(Math.max(
+                    highestProductOf3,
+                    current * highestProductOf2),
+                    current * lowestProductOf2);
+
+            highestProductOf2 = Math.max(Math.max(
+                    highestProductOf2,
+                    current * highest),
+                    current * lowest);
+
+            lowestProductOf2 = Math.min(Math.min(
+                    lowestProductOf2,
+                    current * highest),
+                    current * lowest);
+
+            highest = Math.max(highest, current);
+
+            lowest = Math.min(lowest, current);
+        }
+
+        return highestProductOf3;
     }
 }
--- FindHighestProduct/FindHighestProduct.md
+++ FindHighestProduct/FindHighestProduct.md
@@ -1,3 +1,3 @@
-Given an array of integers, find the highest product you can get from three of the integers.
-
-The input arrayOfInts will always have at least three integers.
+Given an array of integers, find the highest product you can get from three
+integers without using a native sorting method. The solution must be worst
+case O(n) time complexity.
