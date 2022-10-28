bd582ac76a9b9ef5ab6536769937270e98861ad5
Tom Plucinski <tapluci@gmail.com>
2022-10-27 21:43:35 -0700

    Init heap

---
+++ Heaps/MinHeapConstruction/MinHeapConstruction.java
@@ -0,0 +1,34 @@
+static class MinHeap {
+    List<Integer> heap = new ArrayList<Integer>();
+
+    public MinHeap(List<Integer> array) {
+      heap = buildHeap(array);
+    }
+
+    public List<Integer> buildHeap(List<Integer> array) {
+      // Write your code here.
+      return new ArrayList<Integer>();
+    }
+
+    public void siftDown(int currentIdx, int endIdx, List<Integer> heap) {
+      // Write your code here.
+    }
+
+    public void siftUp(int currentIdx, List<Integer> heap) {
+      // Write your code here.
+    }
+
+    public int peek() {
+      // Write your code here.
+      return -1;
+    }
+
+    public int remove() {
+      // Write your code here.
+      return -1;
+    }
+
+    public void insert(int value) {
+      // Write your code here.
+    }
+  }
